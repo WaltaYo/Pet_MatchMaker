67e22b3f54ade5aa94a683b04a8728c67e9d331e
travisruopp <travisruopp@yahoo.com>
2024-04-24 13:06:29 -0500

    More fixes

--- src/main/java/com/petmatchmaking/App.java
+++ src/main/java/com/petmatchmaking/App.java
@@ -1,6 +1,7 @@
-package main.java.com.petmatchmaking;
+package com.petmatchmaking;
 
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App 
