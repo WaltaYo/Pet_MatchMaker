401ae2f8ed9f72dea9581a2f50bcaa6535205737
travisruopp <travisruopp@yahoo.com>
2024-04-24 12:52:25 -0500

    Basic Folder Layout for back end and started folder for front end. Waiting on next week to start frontend folder layout for react purposes.

---
+++ src/test/java/com/petmatchmaking/PetMatchmakingApplicationTests.java
@@ -0,0 +1,11 @@
+package java.com.petmatchmaking;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PetMatchmakingApplicationTests {
+    
+    public static void main(String[] args) {
+		SpringApplication.run(Virtualpet4Application.class, args);
+	}
+}
