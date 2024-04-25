12796f1a28544b4e31e0cf702707f4ae6576d5c5
WTurner3209 <winston0932@gmail.com>
2024-04-25 13:59:40 -0400

    Html footer and header base

---
+++ src/main/java/com/petmatchmaking/Controllers/HomeController.java
@@ -0,0 +1,11 @@
+package com.petmatchmaking.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+    @GetMapping
+    public String getIndex(){
+        return "home/index";
+    }
+}
