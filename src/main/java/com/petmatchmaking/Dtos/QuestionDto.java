20aec804bdaeaf4293a0df4044e26f550be5a2ec
WaltaYo <wyoseph@gmail.com>
2024-04-26 09:44:06 -0400

    question models completed

---
+++ src/main/java/com/petmatchmaking/Dtos/QuestionDto.java
@@ -0,0 +1,80 @@
+package com.petmatchmaking.Dtos;
+
+import com.petmatchmaking.Models.QuestionModel;
+
+/**
+ * Class that models the question model data transfer object
+ */
+public class QuestionDto {
+     
+    private Long id;
+    private String question;
+
+    /**
+     * Default Constructor
+     */
+    public QuestionDto() {
+    }
+
+    /**
+     * Parameterized Constructor
+     * 
+     * @param model Question Model
+     */
+    public QuestionDto(QuestionModel model) {
+        this.id = model.getId();
+        this.question = model.getQuestion();
+    }
+
+    /**
+     * Method to convert the question data transfer object to a question model
+     * 
+     * @return a question model
+     */
+    public QuestionModel convertToModel(){
+        QuestionModel question = new QuestionModel(
+            this.getId(),
+            this.getQuestion());
+            return question;
+    }
+
+    /**
+     * Method to get the question id
+     * 
+     * @return question id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Method to set the Question id
+     * 
+     * @param id Question id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+     /**
+     * Method to get the question
+     * 
+     * @return question
+     */
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * Method to set the question
+     * 
+     * @return question
+     */
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    
+    
+    
+    
+}
---
+++ src/main/java/com/petmatchmaking/Models/QuestionModel.java
@@ -0,0 +1,84 @@
+package com.petmatchmaking.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * Class that models the questions
+ */
+@Entity
+@Table(name = "t_Questions")
+public class QuestionModel {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 900, nullable = false)
+    private String question;
+
+    /**
+     * Default Constructor
+     */
+    public QuestionModel() {
+    }
+
+    /**
+     * Parameterized Constructor
+     * 
+     * @param id       question id
+     * 
+     * @param question question string
+     */
+    public QuestionModel(Long id, String question) {
+        this.id = id;
+        this.question = question;
+    }
+
+    /**
+     * Method to get the question id
+     * 
+     * @return question id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Method to get question
+     * 
+     * @return question id
+     */
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * Method to set id 
+     * 
+     * @param id id for this class
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Method to set question
+     * 
+     * @param question
+     */
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    /**
+     * Override method for toString
+     */
+    @Override
+    public String toString() {
+        return "QuestionModel [id=" + id + ", question=" + question + "]";
+    }
+
+}
